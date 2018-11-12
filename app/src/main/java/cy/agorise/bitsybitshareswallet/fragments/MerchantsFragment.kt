7a5d603815b2a135768a77dd39355c0229d14f07
dtvv <coritocorito@hotmail.com>
2018-11-12 04:21:10 -0600

    Location permission for google maps in merchants screen

--- app/src/main/java/cy/agorise/bitsybitshareswallet/fragments/MerchantsFragment.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/fragments/MerchantsFragment.kt
@@ -1,5 +1,8 @@
 package cy.agorise.bitsybitshareswallet.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,12 +10,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import cy.agorise.bitsybitshareswallet.R
 import cy.agorise.bitsybitshareswallet.viewmodels.MerchantsViewModel
-import kotlinx.android.synthetic.main.fragment_merchants.*
 
 class MerchantsFragment : Fragment() {
+
+    private val REQUEST_LOCATION_PERMISSION = 1
 
     companion object {
         fun newInstance() = MerchantsFragment()
@@ -29,13 +36,62 @@ class MerchantsFragment : Fragment() {
 
         var edtText:EditText = rootView.findViewById(R.id.edtText)
 
-        val ft = activity!!.getSupportFragmentManager().beginTransaction()
-        var mapFragment:MapFragment = MapFragment()
-        mapFragment.edtText = edtText
-        ft.replace(R.id.map, mapFragment)
-        ft.commit()
+
+        /*
+         * Check for CAMERA permission
+         * */
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkPermission()) {
+                // Code for above or equal 23 API Oriented Device
+                // Your Permission granted already .Do next code
+
+                val ft = activity!!.getSupportFragmentManager().beginTransaction()
+                var mapFragment:MapFragment = MapFragment()
+                mapFragment.edtText = edtText
+                ft.replace(R.id.map, mapFragment)
+                ft.commit()
+
+            } else {
+                requestPermission() // Code for permission
+            }
+        } else {
+
+            // Code for Below 23 API Oriented Device
+            // Do next code
+
+            val ft = activity!!.getSupportFragmentManager().beginTransaction()
+            var mapFragment:MapFragment = MapFragment()
+            mapFragment.edtText = edtText
+            ft.replace(R.id.map, mapFragment)
+            ft.commit()
+
+        }
 
         return rootView
+    }
+
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(this!!.activity!!, Manifest.permission.ACCESS_FINE_LOCATION)
+        return if (result == PackageManager.PERMISSION_GRANTED) {
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun requestPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this!!.activity!!, Manifest.permission.CAMERA)) {
+            Toast.makeText(activity, activity!!.getString(R.string.permission_denied_map), Toast.LENGTH_LONG).show()
+
+            /*
+             * Disable the button of the camera visibility
+             * */
+            //disableVisibilityCamera()
+
+        } else {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), REQUEST_LOCATION_PERMISSION)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
