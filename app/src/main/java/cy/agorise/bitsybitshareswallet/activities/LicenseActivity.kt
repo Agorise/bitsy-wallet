b4d15e71570eaffb364711009682ed79cb549fdb
dtvv <coritocorito@hotmail.com>
2018-11-15 04:15:44 -0600

    Integration with CrytoNetAccount

--- app/src/main/java/cy/agorise/bitsybitshareswallet/activities/LicenseActivity.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/activities/LicenseActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import cy.agorise.bitsybitshareswallet.R
 import cy.agorise.bitsybitshareswallet.dao.BitsyDatabase
+import cy.agorise.bitsybitshareswallet.models.CryptoNetAccount
 import cy.agorise.bitsybitshareswallet.repository.RepositoryManager
 import cy.agorise.bitsybitshareswallet.utils.Constants
 import kotlinx.android.synthetic.main.activity_license.*
@@ -19,12 +20,12 @@ class LicenseActivity : CustomActivity(){
 
         setContentView(R.layout.activity_license)
 
-        val acounts  = RepositoryManager.getAccountsRepository(this).getTotalAccounts()
+        val cryptoNetAccount: CryptoNetAccount  = RepositoryManager.getAccountsRepository(this).getCryptoNetLocalAcount()!!
 
         if (PreferenceManager.getDefaultSharedPreferences(this)
                 .getBoolean(Constants.KEY_LICENCE_AGREED, false)) {
 
-            if(acounts > 0) {
+            if(cryptoNetAccount != null) {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
