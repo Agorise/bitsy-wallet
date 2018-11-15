5694ff89ef8f8767425361d6a53b9bb2385f0cfc
dtvv <coritocorito@hotmail.com>
2018-11-15 04:19:49 -0600

    New addAccount methos for the Repository accounts

--- app/src/main/java/cy/agorise/bitsybitshareswallet/repository/AccountRepository.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/repository/AccountRepository.kt
@@ -3,6 +3,7 @@ package cy.agorise.bitsybitshareswallet.repository
 import android.app.Activity
 import cy.agorise.bitsybitshareswallet.dao.BitsyDatabase
 import cy.agorise.bitsybitshareswallet.models.AccountSeed
+import cy.agorise.bitsybitshareswallet.models.CryptoNetAccount
 import cy.agorise.bitsybitshareswallet.utils.AESUtils
 
 class AccountRepository(activity: Activity?) : Repository(activity) {
@@ -42,4 +43,22 @@ class AccountRepository(activity: Activity?) : Repository(activity) {
     }
 
 
+    fun addAccount(accountSeed: AccountSeed) : Long{
+        return db!!.accountSeedDao().insertAccountSeed(accountSeed)
+    }
+
+    fun getCryptoNetLocalAcount() : CryptoNetAccount? {
+        var accounts:List<CryptoNetAccount> = db!!.cryptoNetAccountDao().allCryptoNetAccount
+        var contains:Boolean = accounts.size > 0
+        if(contains){
+           return accounts.get(0)
+        }
+        else{
+            return null
+        }
+    }
+
+    fun addCryptoNetAcount(cryptoNetAccount: CryptoNetAccount){
+        db!!.cryptoNetAccountDao().insertCryptoNetAccount(cryptoNetAccount)
+    }
 }
--- app/src/main/java/cy/agorise/bitsybitshareswallet/repository/RepositoryManager.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/repository/RepositoryManager.kt
@@ -1,6 +1,7 @@
 package cy.agorise.bitsybitshareswallet.repository
 
 import android.app.Activity
+import cy.agorise.bitsybitshareswallet.dao.BitsyDatabase
 
 class RepositoryManager{
 
@@ -8,6 +9,10 @@ class RepositoryManager{
 
         fun getAccountsRepository(acivity:Activity) : AccountRepository {
             return AccountRepository(acivity)
+        }
+
+        fun getDB(activity: Activity):BitsyDatabase{
+            return BitsyDatabase.getAppDatabase(activity!!)!!
         }
     }
 }
