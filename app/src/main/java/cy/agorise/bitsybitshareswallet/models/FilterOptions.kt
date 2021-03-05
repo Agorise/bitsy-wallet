cd9ea387c1432fb0648935354684ff6263649e62
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2021-03-04 22:33:51 -0800

    Migrate to the kotlin-parcelize plugin.
    - Get rid of the deprecated kotlin-android-extensions plugin.

--- app/src/main/java/cy/agorise/bitsybitshareswallet/models/FilterOptions.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/models/FilterOptions.kt
@@ -1,14 +1,14 @@
 package cy.agorise.bitsybitshareswallet.models
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 import cy.agorise.bitsybitshareswallet.fragments.TransactionsFragment
+import kotlinx.parcelize.Parcelize
 
 /**
  * Model that includes all the options to filter the transactions in the [TransactionsFragment]
  */
 @Parcelize
-data class FilterOptions (
+data class FilterOptions(
     var query: String = "",
     var transactionsDirection: Int = 0,
     var dateRangeAll: Boolean = true,
